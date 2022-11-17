82b7019416ad0c13b4d818af26644335e1a8acff
Alexandre Colmenero <alexandrelimaxs@gmail.com>
2022-11-16 21:54:41 -0300

    Função de teste feita

--- hash.cpp
+++ hash.cpp
@@ -1,42 +1,39 @@
 #include "hash.hpp"
-#include "cronometro.hpp"
-// #include <chrono>
-// #include <thread>
+// #include "cronometro.hpp"
 
-// auto inicio(){
-//     auto start = std::chrono::high_resolution_clock::now();
-//     return start;
-// }
 
-// void fim(auto start){
-//     auto end = std::chrono::high_resolution_clock::now();
-//     std::chrono::duration<double, std::milli> float_ms = end - start;
-//     std::cout << "funcSleep() elapsed time is " << float_ms.count() << " milliseconds" << std::endl;
-// }
+// função para criar e popular listas com valores de 0 a 99
+auto populate(int size){
+    vector<int> lista;
+    for (int loop = 0; loop < size; loop++)
+    {
+        int valor = rand() % 100;
+        lista.push_back(valor);
+    }
+    return lista;
+}
+
+void teste_insercao(int quantidade_de_elementos, int tamanho_hash){
+
+    vector valores = populate(quantidade_de_elementos); // já cria o vetor dentro do teste
+
+    // inicio do cronometro de inserção
+
+    Hash tabela_hash(tamanho_hash);
+
+    for (int i = 0; i < valores.size(); i++)
+    {
+        tabela_hash.inserir_dado(valores[i]);
+    }
+    // fim do cronometro de inserção
+
+    tabela_hash.displayHash();
+    
+}
 
 int main(){
 
-    cronometro teste;
-
-      
-    // array that contains keys to be mapped
-    int a[] = {15, 11, 27, 8, 12};
-    int n = sizeof(a)/sizeof(a[0]);
-    // quantidade de bytes total / pela quantidade de bytes de um int = qnt de itens na lista 20/4=5
-
-    // insert the keys into the hash lista
-    Hash h(7); // 7 is count of tamanhos in
-                // hash lista
-    for (int i = 0; i < n; i++)
-        h.inserir_dado(a[i]);
-
-    // delete 12 from hash lista
-    h.apagar_dado(12);
-
-    // display the Hash lista
-    h.displayHash();
-
-    teste.fim();
+    teste_insercao(20,30);
 
     return 0;
 }
